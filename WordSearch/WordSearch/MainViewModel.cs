77ec9df55615bdb2c2dd1f8d6cce4bf34c481479
Alexander Drachnev <ADrachnev@cellent.de>
2019-01-15 21:45:13 +0100

    Display words

--- WordSearch/WordSearch/MainViewModel.cs
+++ WordSearch/WordSearch/MainViewModel.cs
@@ -1,19 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WordsGenerator;
 
 namespace WordSearch
 {
-    public class MainViewModel
+    public class MainViewModel:INotifyPropertyChanged
     {
+        ObservableCollection<Word> _words;
+        private ICommand _command;
 
-        public List<WordsGenerator.Word> Words { get; set; }
-        public MainViewModel()
+        public ObservableCollection<Word> Words
         {
-            Words = WordsGenerator.Generator.GetWords();
-
+            get { return _words; }
+            set
+            {
+                _words = value;
+                NotifyPropertyChanged();
+            }
         }
+        
+
+        public ICommand GenerateWordsCommand
+        {
+            get
+            {
+                return _command ?? (_command = new CommandHandler(() => 
+                {
+                    if (Words == null)
+                        Words = new ObservableCollection<Word>(Generator.GetWords());
+                }, 
+                true));
+            }
+        }
+        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        
     }
+    
+
 }
